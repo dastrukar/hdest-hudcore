91956b2774ef04d109df2a345a8118bdaa70b668
dastrukar <3derid@gmail.com>
2025-01-18 23:07:03 +0800

    compile: add common condition constant

--- modules/constants.py
+++ modules/constants.py
@@ -2,3 +2,4 @@ HUDCORE_INIT_OVERRIDE = 'override void Init(HCStatusbar sb)'
 HUDCORE_DRAW_OVERRIDE = 'override void DrawHUDStuff(HCStatusbar sb, int state, double ticFrac)'
 
 ALWAYS_CONDITION = '(!CheckAlwaysStuff(sb, state, ticFrac))'
+COMMON_CONDITION = '(CheckCommonStuff(sb, state, ticFrac))'
