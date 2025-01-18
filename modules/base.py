3c6f368ce5582f6cb9071559e10178f6498ff431
dastrukar <3derid@gmail.com>
2025-01-19 00:18:12 +0800

    compile: add missing semicolon

--- modules/base.py
+++ modules/base.py
@@ -46,7 +46,7 @@ def generate_init_code(z_layer, namespace):
 		'	override void Init(HCStatusbar sb)',
 		'	{',
 		f'		ZLayer = {z_layer};',
-		f'		Namespace = "{namespace}"',
+		f'		Namespace = "{namespace}";',
 		'	}',
 	])
 
