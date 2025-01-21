55cad331a504a0b8fb0863a793d156f88c347de2
dastrukar <3derid@gmail.com>
2025-01-21 12:58:30 +0800

    compile: show what module is being created

--- compile.py
+++ compile.py
@@ -60,6 +60,7 @@ def main():
 
 	# create module files
 	for mod in MODULES:
+		print(f'> Creating module {mod.class_name}')
 		if mod.search_category not in source_categories:
 			print(f'> Failed to create module {mod.class_name}: Could not find category {mod.search_category}')
 			continue
